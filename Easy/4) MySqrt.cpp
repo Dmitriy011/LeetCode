4c0207b87f9260a89415a6e44b36fe50daec0471
Dmitriy <rozandima01@mail.ru>
2024-05-18 18:38:51 +0300

    add MySqrt

---
+++ Easy/4) MySqrt.cpp
@@ -0,0 +1,36 @@
+﻿# include <cmath>
+
+int mySqrt(int x) {
+    if (x == 0 || x == 1)
+    {
+        return x;
+    }
+
+    int st = 1;
+    int fn = x;
+    int mid;
+
+    while (st <= fn)
+    {
+        mid = (st + fn) * 0.5;
+
+        if ((long)mid * mid > (long)x)
+        {
+            fn = mid - 1;
+            continue;
+        }
+        else
+        {
+            if (mid * mid == x)
+            {
+                return mid;
+            }
+            else
+            {
+                st = mid + 1;
+            }
+        }
+    }
+
+    return round(fn);
+}
